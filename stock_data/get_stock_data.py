791194819e5b18e76b01741426ecfa3466b9112c
enprogames <e_posner@outlook.com>
2021-07-14 20:21:42 -0700

    Created functions for finding slope and average slope of stock graph

--- stock_data/get_stock_data.py
+++ stock_data/get_stock_data.py
@@ -1,26 +1,67 @@
 import datetime
+from typing import List
 
 import yfinance as yf
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import download_data
+
+
+def slope(graph: List[float], point: int):
+    """
+    Find the slope of the graph at a point
+    :param graph: A numpy array containing the y axis of the graph
+    :param point: The point at which the slope will be calculated
+    :return: Slope of graph at given point
+    """
+
+    return graph[point+1] - graph[point]
+
+
+def avg_tan_line(graph: List[float]) -> List[float]:
+
+    tan_line_vals: List[float] = []
+
+    avg_slope: float
+    graph_len: int = len(graph)
+    slope_total: float = 0
+
+    for x_val in range(0, graph_len-1):
+        slope_total += slope(graph, x_val)
+
+    avg_slope = slope_total/graph_len
+
+    for x_val in range(graph_len):
+        # tangent line: y = mx + b
+        print(x_val)
+        tan_line_vals.append(avg_slope*x_val)
+
+    return tan_line_vals
 
 # store ticker values for AMD, Disney, and Tesla
 tickers = ('AMD', 'DIS', 'TSLA')
 
-# get all ticker data for Microsoft
-ticker_data = yf.Ticker('MSFT')
+# get all close data for Microsoft
+msft_close_data = pd.read_pickle('stored_data/msft.pkl').Close
+
+msft_close_list = msft_close_data.tolist()
+
+avg_slope = avg_tan_line(msft_close_list)
 
 # get close prices for tickers
-stock_prices = yf.download(tickers, start="2020-1-1", end=datetime.datetime.now().strftime('%Y-%m-%d'), peroid='ytd').Close
+#stock_prices = yf.download(tickers, start="2020-1-1", end=datetime.datetime.now().strftime('%Y-%m-%d'), peroid='ytd').Close
 
 #print(stock_prices)
 #print(ticker_data.history(period='1d', start='2020-1-1', end=datetime.datetime.now().strftime('%Y-%m-%d')))
 
 
-stock_prices['AMD'].plot()
-stock_prices['DIS'].plot()
-stock_prices['TSLA'].plot()
-ticker_data.history(period='1d', start='2020-1-1', end=datetime.datetime.now().strftime('%Y-%m-%d')).Close.plot(
-    label='MSFT')
+# stock_prices['AMD'].plot()
+# stock_prices['DIS'].plot()
+# stock_prices['TSLA'].plot()
+#msft_close_data.plot(label='MSFT')
+plt.plot(msft_close_list, label='MSFT')
+plt.plot(avg_slope, msft_close_data, label='MSFT avg slope')
 
 plt.title('Stock Prices for AMD, Disney, and Tesla')
 plt.ylabel('Stock Price')
