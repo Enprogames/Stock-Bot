16ea3160a1dca44f2d093f717715758854f44b39
Owner <PosnerEthan@outlook.com>
2021-07-29 17:31:34 -0700

    Created file for testing alpaca api

---
+++ src/alpaca_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+__author__ = "Ethan Posner"
+__copyright__ = ""
+__credits__ = []
+__license__ = ""
+__version__ = "0,1dev"
+__maintainer__ = "Ethan Posner"
+__email__ = "PosnerEthan@outlook.com"
+__status__ = "Production"
+
+import alpaca_trade_api as tradeapi
+
+api = tradeapi.REST()
