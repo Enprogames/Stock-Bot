b362bac9ddea54a761fb1504297825a30f2a65ff
enprogames <e_posner@outlook.com>
2021-07-31 18:44:32 -0700

    Stock data is now stored in stock_data/tickers. Data can also be set to expire after set number of seconds, in which case, new data will be retrieved.

--- src/download_data.py
+++ src/download_data.py
@@ -1,49 +1,66 @@
 import datetime
 import os
+from typing import Dict
 
 import pandas as pd
 import yfinance as yf
 
 
-def store_ticker(ticker_symbol: str):
+class StockDataProvider:
 
-    ticker_directory = os.path.join('stored_data', 'tickers')
+    def __init__(self, tolerance=60*60*24, ticker_directory=os.path.join('stored_data', 'tickers')):
+        """
 
-    if not os.path.exists('stored_data'):
-        os.mkdir('stored_data')
-    if not os.path.exists(ticker_directory):
-        os.mkdir(ticker_directory)
+        :param tolerance: Expiration value for ticker data in seconds. e.g. if stock is more than 300 seconds old, get
+        new data.
+        :param ticker_directory: Location to store pickled stock data in
+        """
 
-    ticker_file_path = os.path.join(ticker_directory, f'{ticker_symbol.upper()}.pkl')
+        self.ticker_directory = ticker_directory
 
-    ticker_data = yf.Ticker(ticker_symbol)
-    ticker_dataframe = ticker_data.history(period='1d', start='2020-1-1', end=datetime.datetime.now()
-                                           .strftime('%Y-%m-%d'))
-    ticker_dataframe.to_pickle(os.path.join(ticker_directory, f'{ticker_symbol.upper()}.pkl'))
+        self.tolerance = tolerance
 
+        self.last_retrieval_time_dict: Dict[str, datetime.datetime] = {}
 
-def get_ticker(ticker_symbol: str, tolerance=300):
-    """
+    def store_ticker(self, ticker_symbol: str):
 
-    :param ticker_symbol: A stock ticker to get data for e.g. 'MSFT'
-    :param tolerance: Expiration value for ticker data in seconds. e.g. if stock is more than 300 seconds old, get
-    new data.
-    :return: Pandas dataframe containing data for the given ticker
-    """
+        # create a new directory to store the data if it doesn't already exist
+        if not os.path.exists('stored_data'):
+            os.mkdir('stored_data')
+        if not os.path.exists(self.ticker_directory):
+            os.mkdir(self.ticker_directory)
 
-    ticker_directory = os.path.join('stored_data', 'tickers')
+        ticker_file_path = os.path.join(self.ticker_directory, f'{ticker_symbol.upper()}.pkl')
 
-    ticker_file_path = os.path.join(ticker_directory, f'{ticker_symbol.upper()}.pkl')
+        # get stock data from yahoo finance
+        ticker_data = yf.Ticker(ticker_symbol)
+        self.last_retrieval_time_dict[ticker_symbol] = datetime.datetime.now()
 
-    # if the ticker has already been stored, see if it is up to date. If not, store it again.
-    if os.path.isfile(ticker_file_path):
-        ticker_dataframe = pd.read_pickle(ticker_file_path)
-        latest_time = ticker_dataframe.index[-1]
-        if latest_time < datetime.datetime.now() - datetime.timedelta(seconds=tolerance):
-            print('new stock data retrieved')
-            store_ticker(ticker_symbol)
-    else:
-        store_ticker(ticker_symbol)
-        ticker_dataframe = pd.read_pickle(ticker_file_path)
+        ticker_dataframe = ticker_data.history(period='max', frequency='1d')
+        ticker_dataframe.to_pickle(ticker_file_path)
 
-    return ticker_dataframe
+    def get_ticker(self, ticker_symbol: str):
+        """
+
+        :param ticker_symbol: A stock ticker to get data for e.g. 'MSFT'
+        :return: Pandas dataframe containing data for the given ticker
+        """
+
+        ticker_file_path = os.path.join(self.ticker_directory, f'{ticker_symbol.upper()}.pkl')
+
+        # if the ticker has already been stored, see if it is up to date. If not, store it again.
+        if os.path.isfile(ticker_file_path):
+            ticker_dataframe = pd.read_pickle(ticker_file_path)
+            if ticker_symbol in self.last_retrieval_time_dict:
+                print(f'latest retrieval time: {self.last_retrieval_time_dict[ticker_symbol]}')
+            print(f'current time minus tolerance: {datetime.datetime.now() - datetime.timedelta(seconds=self.tolerance)}')
+            adjusted_time = datetime.datetime.now() - datetime.timedelta(seconds=self.tolerance)
+            if ticker_symbol in self.last_retrieval_time_dict and \
+                    self.last_retrieval_time_dict[ticker_symbol] < adjusted_time:
+                print('new stock data retrieved')
+                self.store_ticker(ticker_symbol)
+        else:
+            self.store_ticker(ticker_symbol)
+            ticker_dataframe = pd.read_pickle(ticker_file_path)
+
+        return ticker_dataframe
