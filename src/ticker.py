c658b83727cb0b1ba976395e7f8b8a149cc2b892
enprogames <e_posner@outlook.com>
2021-07-31 19:41:19 -0700

    Created Ticker object to store and manipulate stock data

--- src/ticker.py
+++ src/ticker.py
@@ -15,28 +15,55 @@ class Ticker(pd.DataFrame):
 
         self.symbol = symbol
 
-    def moving_avg(self):
+    def update_data(self):
+        """
+        Update this ticker for the latest available data
+        """
         pass
 
-    def slope_at_point(self, graph: np.array, point: int):
+    def slope_at_point(self, point: int) -> float:
         """
         Find the slope of the graph at a point
-        :param graph: A numpy array containing the y axis of the graph
         :param point: The point at which the slope will be calculated
         :return: Slope of graph at given point
         """
+        graph = self.Close.to_array()
 
         return graph[point + 1] - graph[point]
 
-    def moving_avg_line(self, graph: pd.Series) -> pd.Series:
+    def moving_avg(self, start=None, end=None) -> float:
+
+        if not start:
+            start = self.index[0]
+        if not end:
+            end = self.index[-1]
+
+        graph = self.Close.between_time(start=start, end=end, include_start=True, include_end=True)
+
+        avg_slope_val: float
+        graph_len: int = len(graph)
+
+        # find the average slope of the graph
+        avg_slope_val = sum([graph[x_val + 1] - graph[x_val] for x_val in range(0, graph_len - 1)]) / graph_len
+
+        return avg_slope_val
+
+    def moving_avg_line(self, start=None, end=None) -> pd.Series:
+
+        if not start:
+            start = self.index[0]
+        if not end:
+            end = self.index[-1]
+
+        graph: pd.Series = self.Close.between_time(start=start, end=end, include_start=True, include_end=True)
+
         tan_line_vals: np.array = np.empty(len(graph))
 
         avg_slope_val: float
         graph_len: int = len(graph)
 
         # find the average slope of the graph
-        avg_slope_val = sum([self.slope_at_point(graph, x_val) for x_val in range(0, graph_len - 1)]) / graph_len
-        print(avg_slope_val)
+        avg_slope_val = sum([graph[x_val + 1] - graph[x_val] for x_val in range(0, graph_len - 1)]) / graph_len
 
         # find the best y-intercept so that the middle of the equation is at the average height of the graph
         avg_height: float = graph.to_numpy().sum() / graph_len
