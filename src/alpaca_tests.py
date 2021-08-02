0c62ca75d214d3aef82e8a4fbcc2a4e6045ea081
enprogames <e_posner@outlook.com>
2021-08-02 13:13:13 -0700

    Ten trades in account are printed out

--- src/alpaca_tests.py
+++ src/alpaca_tests.py
@@ -9,6 +9,19 @@ __maintainer__ = "Ethan Posner"
 __email__ = "PosnerEthan@outlook.com"
 __status__ = "Production"
 
-import alpaca_trade_api as tradeapi
+import json
 
-api = tradeapi.REST()
+import alpaca_trade_api as trade_api
+
+with open('api_key.json', 'r') as json_file:
+    api_key_json = json.load(json_file)
+
+API_Key_ID = api_key_json['API_Key']
+API_Secret_Key = api_key_json['Secret_Key']
+
+api = trade_api.REST(key_id=API_Key_ID, secret_key=API_Secret_Key)
+
+trades = api.get_trades_iter("MSFT", "2021-02-08", "2021-02-08", limit=10)
+
+for trade in trades:
+    print(trade)
