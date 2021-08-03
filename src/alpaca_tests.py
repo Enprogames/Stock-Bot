d89b2ef538b612c8b0fcd8ae87a70939878ccb55
enprogames <e_posner@outlook.com>
2021-08-02 20:43:35 -0700

    Changed api variables to match the ones provided in the config file

--- src/alpaca_tests.py
+++ src/alpaca_tests.py
@@ -16,11 +16,11 @@ import alpaca_trade_api as trade_api
 with open('api_conf.json', 'r') as json_file:
     api_key_json = json.load(json_file)
 
-API_Key_ID = api_key_json['API_Key']
-API_Secret_Key = api_key_json['Secret_Key']
+API_Key_ID = api_key_json['APCA_API_KEY_ID']
+API_Secret_Key = api_key_json['APCA_API_SECRET_KEY']
 
 # switch to "https://api.alpaca.markets/" for live trading
-base_url = api_key_json['Base_URL']
+base_url = api_key_json['APCA_API_BASE_URL']
 
 api = trade_api.REST(key_id=API_Key_ID, secret_key=API_Secret_Key, base_url=base_url)
 
