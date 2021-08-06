b105b4078777edca078ea5fecf6555a6d80fdc69
enprogames <e_posner@outlook.com>
2021-08-05 19:48:14 -0700

    Added functionality for buying to trading classes. Added stub get_trades and get_symbol_trades methods.

--- src/trade_stocks.py
+++ src/trade_stocks.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Tuple
+from typing import Tuple, List, Dict
 
 import alpaca_trade_api
 
@@ -16,23 +16,33 @@ class StockTraderADT(ABC):
         pass
 
     @abstractmethod
-    def buy(self, symbol: str, amount: float) -> bool:
+    def buy(self, symbol: str, amount: float, notional: bool = False) -> bool:
         """
 
         :param symbol: Ticker symbol of stock to buy
         :param amount: How many stocks to buy
+        :param notional: Whether or not to trade with dollar amount instead of number of stocks
         :return: Whether or not the trade was a success
         """
         pass
 
     @abstractmethod
-    def sell(self, symbol: str, amount: float) -> bool:
+    def sell(self, symbol: str, amount: float, notional: bool = False) -> bool:
         """
 
         :param symbol: Ticker symbol of stock to sell
         :param amount: How many stocks to sell
+        :param notional: Whether or not to trade with dollar amount instead of number of stocks
         :return: Whether or not the trade was a success
         """
+        pass
+
+    @abstractmethod
+    def get_symbol_trades(self, symbol) -> List:
+        pass
+
+    @abstractmethod
+    def get_trades(self) -> List:
         pass
 
 
@@ -41,10 +51,34 @@ class AlpacaStockTrader(StockTraderADT, alpaca_trade_api.REST):
     def __init__(self, credentials: Tuple, base_url):
         alpaca_trade_api.REST.__init__(self, key_id=credentials[0], secret_key=credentials[1], base_url=base_url)
 
-    def buy(self, symbol: str, amount: float) -> bool:
+    def buy(self, symbol: str, amount: float, notional: bool = False) -> bool:
+        result = False
+        self.submit_order(symbol, qty=amount)
+
+    def sell(self, symbol: str, amount: float, notional: bool = False) -> bool:
         pass
 
-    def sell(self, symbol: str, amount: float) -> bool:
+    def get_trades(self):
+        pass
+
+
+class SimulatedTrader(StockTraderADT):
+    """
+    Used for testing trading algorithms on historical stock data
+
+    Since
+    """
+
+    def __init__(self, credentials: Tuple):
+        pass
+
+    def buy(self, symbol: str, amount: float, notional: bool = False) -> bool:
+        pass
+
+    def sell(self, symbol: str, amount: float, notional: bool = False) -> bool:
+        pass
+
+    def get_trades(self):
         pass
 
 
@@ -65,14 +99,35 @@ class StockTrader(StockTraderADT):
         if trade_api == 'alpaca':
             self.trade_api = AlpacaStockTrader(credentials, base_url="https://paper-api.alpaca.markets/")
 
-    def buy(self, symbol: str, amount: float) -> bool:
+        # all trades made by this object
+        self.trades: List = []
+
+        # trades made for different symbols
+        self.symbol_trades: Dict[str, List] = {}
+
+    def buy(self, symbol: str, amount: float, notional: bool = False) -> bool:
 
         # buy stocks using alpaca
         if self.trade_api == 'alpaca':
-            return True
+            return self.trade_api.buy(symbol, amount)
+        return False
 
-    def sell(self, symbol: str, amount: str) -> bool:
+    def sell(self, symbol: str, amount: float, notional: bool = False) -> bool:
+
+        result = False
 
         # sell stocks which were bought using alpaca
         if self.trade_api == 'alpaca':
-            return True
+            result = self.trade_api.sell(symbol, amount)
+            if result:
+                # TODO: Find way of adding trade to list if it was successful
+                self.trades.append(None)
+        return False
+
+    def get_symbol_trades(self, symbol: str):
+        return self.symbol_trades[symbol]
+
+    def get_trades(self):
+        return self.trades
+
+
