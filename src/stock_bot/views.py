ee22f97667084e554da2f633110fe1cbefed42f1
enprogames <e_posner@outlook.com>
2021-08-22 12:17:30 -0700

    Reverted transformation into django project. This has been moved into its own repo.

--- src/stock_bot/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpRequest
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'stock_bot/index.html')
--- src/stock_data/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stock_data'
