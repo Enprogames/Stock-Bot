616e4921895c28413cbe4b3d6f0b8416afb27f64
ethan <e_posner@outlook.com>
2021-08-27 16:14:27 -0700

    Added most setup functionality to setup.sh

--- setup.sh
+++ setup.sh
@@ -10,12 +10,26 @@ function report_result() {
     fi
 }
 
-# venv
-echo -e "\n*** Activating the python virtual environment for this script..."
 OS=$(uname -a | cut -c1-5)
-report_result
+# venv
+## create venv
+echo -e "\n*** Checking to see if virtual environment exists"
+if [[ -d "/venv" ]]; then
+    echo -e "\n*** Creating new virtual environment using python-venv"
+    if [[ $OS =~ "Linux" ]]; then
+        echo "Making adjustments due to bad choice of operating system"
+        python3 -m venv venv --prompt stock-bot
+    else
+        python -m venv venv --prompt stock-bot
+    fi
+    report_result
+else
+    echo -e "\n Virtual environment already exists in this directory."
+fi
+
+## activate venv
+echo -e "\n*** Activating the python virtual environment for this script..."
 if [[ $OS =~ "Linux" ]]; then
-    echo "Making adjustments due to bad choice of operating system"
     source venv/bin/activate
 else
     source venv/Scripts/activate
@@ -51,7 +65,6 @@ cat > src/api_conf.json << EOF
   "APCA_RETRY_CODES": -1,
   "DATA_PROXY_WS": ""
 }
-
 EOF
 
 # setup post-merge hook
@@ -59,9 +72,36 @@ echo -e "\n*** Installing post-merge hooks"
 cat > .git/hooks/post-merge << EOF
 #!/bin/bash
 
-# Run on successful merge
-echo -e "\n*** Running post-merge hook\n"
+# This script will run on a successful merge
 
+echo -e "\n*** Running post-merge hook\n"
+# venv
+echo -e "\n*** Activating the python virtual environment for this script..."
+OS=$(uname -a | cut -c1-5)
+report_result
+if [[ $OS =~ "Linux" ]]; then
+    echo "Making adjustments due to bad choice of operating system"
+    source venv/bin/activate
+else
+    source venv/Scripts/activate
+fi
+report_result
+echo -e "Using: $(which python)"
+
+# upgrade pip
+echo -e "\n*** Updating pip to latest version...\n"
+python -m pip install --upgrade pip
+report_result
+
+# pip install
+echo -e "\n*** Installing latest Python requirements...\n"
+python -m pip install -r requirements.txt
+report_result
+
+# pre-commit install
+echo -e "\n*** Installing any new pre-commit hooks\n"
+python -m pre_commit install
+report_result
 EOF
 report_result
 
