77eea76ce24088d71e8bc8699b642b7cacc229d0
ethan <e_posner@outlook.com>
2022-01-08 17:01:39 -0800

    Created trading_alg_driver.py

---
+++ src/trading_alg_driver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+__author__ = "Ethan Posner"
+__copyright__ = ""
+__credits__ = []
+__license__ = ""
+__version__ = "0,1dev"
+__maintainer__ = "Ethan Posner"
+__status__ = "Production"
+
+# import data_provider as dp
+# import trade_stocks as ts
+
+on = True
+
+while on:
+    pass
